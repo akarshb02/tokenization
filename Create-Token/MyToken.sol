0a2f0e8316b09f406d720e78d50bbef2d4fc4a43
Akarsh <akarshb02@gmail.com>
2021-01-29 22:32:19 +0530

    Delete Create-Token directory

--- Create-Token/MyToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.7.4;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) public ERC20("StarDucks Cappucino Token", "CAPPU") {
-        _mint(msg.sender, initialSupply);
-    }
-}
